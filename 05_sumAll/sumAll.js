87f8626b0898000819c5960fe94f8023212c4a6e
noobwale <wallacesheundah@gmail.com>
2023-05-10 23:16:22 +0300

    solved sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let total = 0;
 
+    if (a < 0 || b < 0) return 'ERROR';
+    if (isNaN(a) || isNaN(b)) return 'Error';
+
+    let min = Math.min(a,b);
+    let max = Math.max(a,b);
+
+    for (let i = min; i <= max; i++) {
+        total= total + i;    
+    };
+    return total;
 };
 
 // Do not edit below this line
